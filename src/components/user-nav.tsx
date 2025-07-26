c5aa31ad08e935cd3ebc0fb286e504a41dec2e42
Hasan Ayub <hasanayub106@gmail.com>
2025-07-26 10:11:01 +0000

    as you can see its showing fake data change it to real email id which i

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -40,9 +40,9 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{user?.name || "User"}</p>
+            <p className="text-sm font-medium leading-none">{user?.name}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              {user?.email || "user@example.com"}
+              {user?.email}
             </p>
           </div>
         </DropdownMenuLabel>
