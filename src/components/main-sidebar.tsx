83312a7997d5eb4a4b5f2a405621311c60365cfb
Hasan Ayub <hasanayub106@gmail.com>
2025-07-26 20:05:17 +0000

    logo finally updated

--- src/components/main-sidebar.tsx
+++ src/components/main-sidebar.tsx
@@ -32,11 +32,12 @@ export function MainSidebar() {
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <Link
           href="/dashboard"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
         >
-          <Icons.logo className="h-5 w-5 transition-all group-hover:scale-110" />
-          <span className="sr-only">Cashflow Compass</span>
+          <img src="/favicon.ico" alt="Cashflow Compass Logo" className="h-5 w-5" />
+          <span className="sr-only">Dashboard</span>
         </Link>
+
         <TooltipProvider>
           {navItems.map((item) => (
             <Tooltip key={item.href}>
